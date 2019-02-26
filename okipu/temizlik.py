e9b07c3fc857cec1e6b33c7f03c40e5ed0a6c78c
onsel <onselaydin@gmail.com>
2019-02-26 23:58:42 +0300

    update query

--- okipu/temizlik.py
+++ okipu/temizlik.py
@@ -1,27 +1,14 @@
 import pyodbc
 import clearmod
-
-'''
-b = clearmod.clear("Sakarya &Uuml;niversitesi Mekatronik Mühendisliği")
-print (b)
-'''
-
-conn = pyodbc.connect('DRIVER={SQL Server};SERVER=mssql11.turhost.com;DATABASE=Okipu101_db;UID=okipusa;PWD=u5C/4Sc}')
+driver = '/usr/local/lib/libtdsodbc.so'
+#conn = pyodbc.connect('DRIVER={SQL Server};SERVER=mssql11.turhost.com;DATABASE=Okipu101_db;UID=okipusa;PWD=u5C/4Sc}')
+conn = pyodbc.connect('DRIVER={SQL Server};SERVER=mssql11.turhost.com;PORT=1433;DATABASE=Okipu101_db;UID=okipusa;PWD=u5C/4Sc}')
 cursor = conn.cursor()
-
-
-
-cursor.execute("select top 10 Title, Writer, Translator, Isbn, Comment from BOOKS")
+cursor.execute("select top 10 Id, Title, Writer, Translator, Isbn, Comment from BOOKS")
 books = cursor.fetchall()
 
-for book in books:
-    a = clearmod.clear(book[0])
-    print(a)
-    '''
-    b = clearmod.clear(book[1])
-    print(b)
-    c = clearmod.clear(book[2])
-    print(c)
-    '''
-#for i,j in donustur.items():
-#    print(i,j)
+
+for i in books:
+    print(i)
+
+conn.close()
