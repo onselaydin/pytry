b7f6b28871ee653c463f1fc8a4a66aba07608566
Önsel AYDIN <onselaydin@gmail.com>
2020-01-10 14:17:17 +0300

    fix basfexport

--- mongo/tarbilRRSort.py
+++ mongo/tarbilRRSort.py
@@ -1,8 +1,8 @@
 from pymongo import MongoClient
 client = MongoClient('10.16.15.89', 27017)
 client.admin.authenticate('agros', 'agros1234', mechanism = 'SCRAM-SHA-1', source='agros')
-mydb = client['AGRO']
-sale = mydb["customer"]
-total = sale.find({"name":{"$regex":"^TAŞ"}})
+mydb = client['LOS']
+sale = mydb["tarbilRr"]
+total = sale.find().sort({"time":-1}).limit(100)
 for x in total:
     print(x)
